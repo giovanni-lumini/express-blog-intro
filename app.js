b47e022e4aac21a3e6c8d886e995760eee104407
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-30 15:21:42 +0100

    controllers

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const port = 3000
 
 app.use(express.static("public")); //alla funzione static diamo il nome della cartella
 
-/* app.get('/', (req, res) => {    //definiamo le rotte
+app.get('/', (req, res) => {    //definiamo le rotte
   res.send('Hello World!')
-}) */
+})
 
 app.listen(port, () => {    //avviamo il server
   console.log(`Example app listening on port ${port}`)
@@ -16,3 +16,6 @@ app.get('/', (req, res) => {    //definiamo le rotte
   let markup = `<h1>Benvenuto nel mio blog!</h1>`
   res.send(markup)
 })
+
+const posts = require("./controllers/posts.js");
+app.get("/posts", posts.index);
---
+++ controllers/posts.js
@@ -0,0 +1,40 @@
+/* 
+Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+*/
+
+const posts = [
+    {
+        titolo: "post 1",
+        contenuto: "contenuto del post 1",
+        immagine: "/public/img1",
+        tags: ["tag1", "tag2", "tag3", "tag4"]
+    },
+    {
+        titolo: "post 2",
+        contenuto: "contenuto del post 2",
+        immagine: "/public/img2",
+        tags: ["tag1", "tag2", "tag3", "tag4"]
+    },
+    {
+        titolo: "post 3",
+        contenuto: "contenuto del post 3",
+        immagine: "/public/img3",
+        tags: ["tag1", "tag2", "tag3", "tag4"]
+    },
+    {
+        titolo: "post 4",
+        contenuto: "contenuto del post 4",
+        immagine: "/public/img4",
+        tags: ["tag1", "tag2", "tag3", "tag4"]
+    },
+    {
+        titolo: "post 5",
+        contenuto: "contenuto del post 5",
+        immagine: "/public/img5",
+        tags: ["tag1", "tag2", "tag3", "tag4"]
+    }
+]
+
+module.exports.index = function (req, res){
+    res.send(posts)
+}
