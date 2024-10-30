8ad5b77e9c72e796e28cc5c9f12aa46ae7b80858
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-30 14:52:19 +0100

    start

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')    //importiamo express
+const app = express()   //inizializziamo express
+const port = 3000
+
+app.use(express.static("public")); //alla funzione static diamo il nome della cartella
+
+/* app.get('/', (req, res) => {    //definiamo le rotte
+  res.send('Hello World!')
+}) */
+
+app.listen(port, () => {    //avviamo il server
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/', (req, res) => {    //definiamo le rotte
+  let markup = `<h1>Benvenuto nel mio blog!</h1>`
+  res.send(markup)
+})
